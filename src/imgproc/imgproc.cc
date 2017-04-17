41fb30589768b1aa3ae9935fcaa3f7af00493cd2
xudafeng <xudafeng@126.com>
2017-04-17 14:48:00 +0800

    fix overflow bug

--- src/imgproc/imgproc.cc
+++ src/imgproc/imgproc.cc
@@ -20,54 +20,60 @@ NAN_METHOD(imgproc::matchTemplate) {
   
   int method = info[2]->Uint32Value();
   Local<Function> cb = Local<Function>::Cast(info[3]);
+
   Local<Value> argv[2];
   argv[0] = Nan::Null();
   
-  try {
-    Local<Object> out = Nan::New(Mat::constructor)->GetFunction() -> NewInstance(v8::Isolate::GetCurrent() -> GetCurrentContext()).FromMaybe(v8::Local<v8::Object>());
-    
-    Mat *m_out = Nan::ObjectWrap::Unwrap<Mat>(out);
-    
-    int cols = img1->mat.cols - img2->mat.cols + 1;
-    int rows = img1->mat.rows - img2->mat.rows + 1;
-    m_out->mat.create(cols, rows, CV_64FC1);
-    cv::matchTemplate(img1->mat, img2->mat, m_out->mat, method);
-    cv::normalize(m_out->mat, m_out->mat, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
-    double minVal;
-    double maxVal;
-    cv::Point minLoc;
-    cv::Point maxLoc;
-    cv::Point matchLoc;
-    
-    minMaxLoc(m_out->mat, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
-    
-    if(method  == CV_TM_SQDIFF || method == CV_TM_SQDIFF_NORMED) {
-      matchLoc = minLoc;
-    } else {
-      matchLoc = maxLoc;
+  Mat dst;
+  int result_cols = img1->mat.cols - img2->mat.cols + 1;
+  int result_rows = img1->mat.rows - img2->mat.rows + 1;
+  
+  if (result_cols > 0 && result_rows > 0) {
+    try {
+      Local<Object> out = Nan::New(Mat::constructor)->GetFunction() -> NewInstance(v8::Isolate::GetCurrent() -> GetCurrentContext()).FromMaybe(v8::Local<v8::Object>());
+      Mat *dst = Nan::ObjectWrap::Unwrap<Mat>(out);
+      dst->mat.create(result_rows, result_cols, CV_32FC1);
+      cv::matchTemplate(img1->mat, img2->mat, dst->mat, method);
+      cv::normalize(dst->mat, dst->mat, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+      double minVal;
+      double maxVal;
+      cv::Point minLoc;
+      cv::Point maxLoc;
+      cv::Point matchLoc;
+      
+      minMaxLoc(dst->mat, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
+      
+      if (method  == CV_TM_SQDIFF || method == CV_TM_SQDIFF_NORMED) {
+        matchLoc = minLoc;
+      } else {
+        matchLoc = maxLoc;
+      }
+      
+      dst->mat.convertTo(dst->mat, CV_8UC1, 255, 0);
+      
+      //detected ROI
+      unsigned int roi_x = matchLoc.x;
+      unsigned int roi_y = matchLoc.y;
+      unsigned int roi_width = img2->mat.cols;
+      unsigned int roi_height = img2->mat.rows;
+      
+      v8::Local <v8::Array> arr = Nan::New<v8::Array>(method);
+      arr->Set(0, out);
+      arr->Set(1, Nan::New<Number>(roi_x));
+      arr->Set(2, Nan::New<Number>(roi_y));
+      arr->Set(3, Nan::New<Number>(roi_width));
+      arr->Set(4, Nan::New<Number>(roi_height));
+      argv[1] = arr;
+      
+    } catch (cv::Exception& e) {
+      argv[0] = Nan::Error(e.what());
+      argv[1] = Nan::Null();
     }
-    
-    //detected ROI
-    unsigned int roi_x = matchLoc.x;
-    unsigned int roi_y = matchLoc.y;
-    unsigned int roi_width = img2->mat.cols;
-    unsigned int roi_height = img2->mat.rows;
-    
-    m_out->mat.convertTo(m_out->mat, CV_8UC1, 255, 0);
-    
-    v8::Local <v8::Array> arr = Nan::New<v8::Array>(method);
-    arr->Set(0, out);
-    arr->Set(1, Nan::New<Number>(roi_x));
-    arr->Set(2, Nan::New<Number>(roi_y));
-    arr->Set(3, Nan::New<Number>(roi_width));
-    arr->Set(4, Nan::New<Number>(roi_height));
-    
-    argv[1] = arr;
-
-  } catch (cv::Exception& e) {
-    argv[0] = Nan::Error(e.what());
+  } else {
+    argv[0] = Nan::Error("one of the size values (row or column) is lower than 0");
     argv[1] = Nan::Null();
   }
+
   Nan::TryCatch try_catch;
   cb->Call(Nan::GetCurrentContext()->Global(), 2, argv);
   
